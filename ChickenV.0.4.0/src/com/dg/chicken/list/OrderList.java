4bc37465f7a568426b08172681c8da2bb1248d85
kimaxee <95838445+kimaxee@users.noreply.github.com>
2022-01-27 10:18:49 +0900

    Delete ChickenV.0.4.0 directory

--- ChickenV.0.4.0/src/com/dg/chicken/list/OrderList.java
+++
@@ -1,9 +0,0 @@
-package com.dg.chicken.list;
-
-import java.util.ArrayList;
-
-import com.dg.chicken.data.Product;
-
-public class OrderList {
-	public static ArrayList<Product> products = new ArrayList<Product>();
-}
