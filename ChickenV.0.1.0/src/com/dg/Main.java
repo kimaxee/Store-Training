bbb08ac3d46ed7787e5dded95adc17abc77097b5
kimaxee <95838445+kimaxee@users.noreply.github.com>
2022-01-27 10:18:20 +0900

    Delete ChickenV.0.1.0 directory

--- ChickenV.0.1.0/src/com/dg/Main.java
+++
@@ -1,8 +0,0 @@
-package com.dg;
-
-public class Main {
-	public static void main(String[] args) {
-		DgChicken dgChicken = new DgChicken();
-		dgChicken.proc();
-	}
-}
--- ChickenV.0.1.0/src/com/dg/chicken/diaplay/Title.java
+++
@@ -1,8 +0,0 @@
-package com.dg.chicken.diaplay;
-
-public class Title {
-	public static final String TITLE = 
-			"****************************************\n"
-		   +"*********    CHICKEN V.0.1.0   *********\n"	   
-		   +"****************************************";;
-}
